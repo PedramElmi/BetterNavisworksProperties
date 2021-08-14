6e72c6a6e54726c9dbe6d240bbc66bde4d9e0124
Pedram Elmi <pedram.elmi@gmail.com>
2021-08-14 17:26:19 +0430

    Add project files.

---
+++ BetterPropertiesDockpane/BetterPropertiesDockPanePlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Plugins;
+using System.Windows.Forms;
+
+namespace BetterPropertiesDockpane
+{
+    [Plugin(name: "BetterProperties", developerId: "PDEL", DisplayName = "Better Properties")]
+    [DockPanePlugin(preferredWidth: 400, preferredHeight: 600, AutoScroll = true, FixedSize = false, MinimumHeight = 500, MinimumWidth = 200)]
+    public class BetterPropertiesDockPanePlugin : DockPanePlugin
+    {
+        public override Control CreateControlPane()
+        {
+            return base.CreateControlPane();
+        }
+    }
+}
