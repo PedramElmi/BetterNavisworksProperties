7959620ac83f218fe96d6decbd68ad2451ab5f79
Pedram Elmi <pedram.elmi@gmail.com>
2021-08-21 10:57:48 +0430

    Added Save as json Option. Selected elements's properties can now be saved.

--- BetterPropertiesDockpane/MVVM/ViewModels/BetterPropertiesViewModel.cs
+++ BetterPropertiesDockpane/MVVM/ViewModels/BetterPropertiesViewModel.cs
@@ -1,6 +1,11 @@
 ﻿using Autodesk.Navisworks.Api;
 using BetterPropertiesDockpane.Helper;
 using System;
+using Forms = System.Windows.Forms;
+using System.Windows.Input;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using Windows = System.Windows;
 
 namespace BetterPropertiesDockpane.MVVM.ViewModels
 {
@@ -17,6 +22,9 @@ namespace BetterPropertiesDockpane.MVVM.ViewModels
         }
 
 
+        public ICommand SaveAsJsonFileCommand { get; set; }
+
+
 
 
 
@@ -24,8 +32,70 @@ namespace BetterPropertiesDockpane.MVVM.ViewModels
         {
             Application.ActiveDocument.CurrentSelection.Changed += this.CurrentSelection_Changed;
             Application.ActiveDocumentChanged += this.CurrentSelection_Changed;
+
+            SaveAsJsonFileCommand = new RelayCommand(SaveAsJsonFile, CanBeSavedAsJsonFile);
+
         }
 
+        public void SaveAsJsonFile(object _)
+        {
+
+            Forms.SaveFileDialog saveFileDialog = new Forms.SaveFileDialog()
+            {
+                Filter = "JavaScript Object Notation|* json",
+                Title = "Save Selected ModelItems Properties"
+            };
+
+            if (saveFileDialog.ShowDialog() == Forms.DialogResult.OK)
+            {
+
+                var output = NavisworksDevHelper.ModelItem.CategoriesPropertiesHelper.SerializeModelItemsProperties(SelectedModelItems);
+
+                string pattern = @"^.*\.(json)$";
+
+                try
+                {
+                    if (!Regex.IsMatch(saveFileDialog.FileName, pattern))
+                    {
+                        System.IO.File.WriteAllText($"{saveFileDialog.FileName}.json", output.ToString());
+                    }
+                    else
+                    {
+                        System.IO.File.WriteAllText(saveFileDialog.FileName, output.ToString());
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    var result = Windows.MessageBox.Show(e.Message, e.StackTrace, Windows.MessageBoxButton.OKCancel);
+                    if (result == Windows.MessageBoxResult.OK)
+                    {
+                        SaveAsJsonFile(_);
+                    }
+                }
+                
+            }
+        }
+        
+        private bool CanBeSavedAsJsonFile(object _)
+        {
+
+            if (SelectedModelItems is null)
+            {
+                return false;
+            }
+            else if (SelectedModelItems.Count != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        
         private void CurrentSelection_Changed(object sender, EventArgs e)
         {
             // if a real document still not loaded then do nothing
@@ -33,6 +103,7 @@ namespace BetterPropertiesDockpane.MVVM.ViewModels
             {
                 SelectedModelItems = ((Document)sender).CurrentSelection.SelectedItems;
             }
-        }
+        } 
+
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Better Properties Window for Navisworks
 **Goal:** having a window better than the built-in Properties DockPane
 
- - Under developement
-
 ![Sample](https://github.com/PedramElmi/BetterNavisworksProperties/raw/main/Resources/Screenshot.png)
